8f54a99e3657b4cf8cfb3badafe4dff8e2be32b2
sat2llite <raumschiff_@naver.com>
2023-07-10 18:08:41 +0900

    menu 수정

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import { Navigate } from 'react-router'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
     <div className="menu">
       <ul className="menu_ul flex_center">
-        <li>Profile</li>
-        <li>Skills</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/profile">
+          <li>Profile</li>
+        </Link>
+        <Link to="/skills">
+          <li>Skills</li>
+        </Link>
+        <Link to="/portfolio">
+          <li>Portfolio</li>
+        </Link>
+        <Link to="/contact">
+          <li>Contact</li>
+        </Link>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
